c324fe572591591d692c22d49314dc04be18905f
SupSyDeMarciou <130789459+SupSyDeMarciou@users.noreply.github.com>
2024-09-28 20:10:14 +0200

    Added "SL.h" and rearranged layout

---
+++ include/SL.h
@@ -0,0 +1,20 @@
+#ifndef __SUPSY_SL
+#define __SUPSY_SL
+
+#include "SL/structures.h"
+
+#include "SL/maths/constants.h"
+#include "SL/maths/math.h"
+#include "SL/maths/vector.h"
+#include "SL/maths/quaternion.h"
+#include "SL/maths/matrix.h"
+
+#include "SL/utils/inout.h"
+#include "SL/utils/list.h"
+#include "SL/utils/array.h"
+#include "SL/utils/debug.h"
+#include "SL/utils/imageImporter.h"
+#include "SL/utils/arenaAlloc.h"
+#include "SL/utils/hashtbl.h"
+
+#endif
